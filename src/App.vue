04746ff7ad97c78a5132476a8d64c7aeb4472a18
Joaquim Filho <joaquim.filho@sgnsistemas.com>
2023-06-14 16:45:35 -0300

    Initial project

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="caixa">
+        <header>
+            <img src="./assets/NID-banner.png" alt="NID-banner">
+            <hr>
+        </header>
+
+        <div id="formulario">
+            <div class="mt-4">
+                <p class="h1 text-center">LOGIN</p>  
+            </div>
+
+            <form class="mt-4">
+                <div class="form-outline mb-4 h5">
+                    <label class="form-label" for="form2Example1">Usuário</label>
+                    <input type="email" id="form2Example1" class="form-control rounded-pill" />
+                </div>
+              
+                <div class="form-outline mb-4 h5">
+                    <label class="form-label" for="form2Example2">Senha</label>
+                    <input type="password" id="form2Example2" class="form-control rounded-pill" />
+                </div>
+
+                <div class="d-flex justify-content-center">
+                    <input type="submit" class="btn rounded-pill btn-lg px-5" />
+                </div>
+            </form>
+        </div>
+    </div>   
+</template>
+
+<style lang="scss">
+
+// @import '@/styles/ltd.scss';
+// @import '@/styles/login.scss';
+@import '@/styles/login.css'
+
+
+</style>
---
+++ src/components/tab.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="events">
+            <h3>Calendário de eventos</h3>
+
+            <div id="calendar">
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Data</th>
+                            <th>Evento</th>
+                            <th>Horário inicio</th>
+                            <th>Horário encerramento</th>
+                            <th>Sala</th>
+                            <th>Status</th>
+                        </tr>
+        
+                        <tr v-for="evento in eventos" :key="evento.id">
+                            <td>{{ evento.data }}</td>
+                            <td>{{ evento.name }}</td>
+                            <td>{{ evento.horario_inicio }}</td>
+                            <td>{{ evento.horario_final }}</td>
+                            <td>{{ evento.sala }}</td>
+                            <td>{{ evento.status }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HelloWorld',
+    props: {
+      evento: Array
+    }
+  }
+  </script>
+  
